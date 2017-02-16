05cf9401213189f38556c3fcf8946c4b9b6e8687
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2017-02-16 15:49:56 +0100

    refactors to use arrays over string concatenation, also fixes some stuff that shellcheck complains about

--- composer.sh
+++ composer.sh
@@ -15,65 +15,59 @@ IMAGE="torvitas/composer:$VERSION"
 
 # You can set an environment file to be used using the $ENVIRONMENT_FILE
 # environment variable.
-ENVIRONMENT_FILE=${ENVIRONMENT_FILE:-$PWD/.env}
-
+ENVIRONMENT_FILE="${ENVIRONMENT_FILE:-$PWD/.env}"
 if [ -n "$ENVIRONMENT_FILE" ]; then
-    source $ENVIRONMENT_FILE
+    source "$ENVIRONMENT_FILE"
 fi
 
 # You can add additional volumes (or any docker run options) using
 # the $COMPOSER_OPTIONS environment variable.
-COMPOSER_OPTIONS=${COMPOSER_OPTIONS:-}
-
-# You can set a folder to be mapped to /root/.ssh using the
-# $SSH_DIR environment variable.
-SSH_DIR=${SSH_DIR:-}
+COMPOSER_OPTIONS="${COMPOSER_OPTIONS:-}"
 
 # if we can't find an agent, start one, and restart the script.
 if [ -z "$SSH_AUTH_SOCK" ] ; then
-    eval $(ssh-agent)
+    eval "$(ssh-agent)"
 fi
+
+VOLUMES=()
 # Setup volume mounts for composer config, context and ssh keys
 if [ "$(pwd)" != '/' ]; then
-    VOLUMES="-v $(pwd):$(pwd)"
+    VOLUMES+=(-v "$(pwd):$(pwd)")
+fi
+VOLUMES+=(-v "$(readlink -f "$SSH_AUTH_SOCK"):/ssh-agent")
+if [ -e "/etc/shadow" ]; then
+    VOLUMES+=(-v "/etc/passwd:/etc/passwd:ro")
+fi
+if [ -e "/etc/passwd" ]; then
+    VOLUMES+=(-v "/etc/shadow:/etc/shadow:ro")
 fi
 
 # You can set the composer cache directory using the $COMPOSER_CACHE
 # environment variable.
-COMPOSER_CACHE=${COMPOSER_CACHE:-$PWD/tmp/composer}
+COMPOSER_CACHE="${COMPOSER_CACHE:-$PWD/tmp/composer}"
 if [[ -n "$COMPOSER_CACHE" ]]; then
-    mkdir -p "$COMPOSER_CACHE";
-    VOLUMES="$VOLUMES -v $(cd $COMPOSER_CACHE; pwd):/composer/cache"
+    mkdir -p "$COMPOSER_CACHE"
+    VOLUMES+=(-v "$(cd $COMPOSER_CACHE; pwd):/composer/cache")
 fi
 
 if [ -n "$COMPOSER_FILE" ]; then
-    composer_dir=$(dirname $COMPOSER_FILE)
+    composer_dir="$(dirname $COMPOSER_FILE)"
 fi
 if [[ -n "$composer_dir" && -d "$composer_dir" ]]; then
-    VOLUMES="$VOLUMES -v $(cd $composer_dir; pwd):$composer_dir"
+    VOLUMES+=(-v "$(readlink -f $composer_dir):$composer_dir")
 fi
-if [ -n "$SSH_DIR" ]; then
-    mkdir -p "$SSH_DIR";
-    VOLUMES="$VOLUMES -v $(cd $SSH_DIR; pwd):/root/.ssh"
-fi
-if [ -e "./data/etc/ssh/known_hosts" ]; then
-    VOLUMES="$VOLUMES -v $(pwd)/data/etc/ssh/known_hosts:$HOME/.ssh/known_hosts"
-fi
-if [ -e "/etc/shadow" ]; then
-    VOLUMES="$VOLUMES -v /etc/shadow:/etc/shadow"
-fi
-if [ -e "/etc/passwd" ]; then
-    VOLUMES="$VOLUMES -v /etc/passwd:/etc/passwd"
+SSH_KNOWN_HOSTS="${SSH_KNOWN_HOSTS:-$(pwd)/data/etc/ssh/known_hosts}"
+if [ -e "$SSH_KNOWN_HOSTS" ]; then
+    VOLUMES+=(-v "$SSH_KNOWN_HOSTS:$HOME/.ssh/known_hosts")
 fi
 # Only allocate tty if we detect one
 if [ -t 1 ]; then
-    DOCKER_RUN_OPTIONS="-t"
+    DOCKER_RUN_OPTIONS+=(-t)
 fi
 if [ -t 0 ]; then
-    DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -i"
+    DOCKER_RUN_OPTIONS+=(-i)
 fi
 exec docker run --rm \
-    -u $(id -u) \
+    -u "$(id -u):$(id -g)" \
     -e SSH_AUTH_SOCK=/ssh-agent \
-    -v $(readlink -f $SSH_AUTH_SOCK):/ssh-agent \
-    $DOCKER_RUN_OPTIONS $COMPOSER_OPTIONS $VOLUMES -w "$(pwd)" $IMAGE "$@"
+    "${DOCKER_RUN_OPTIONS[@]}" $COMPOSER_OPTIONS "${VOLUMES[@]}" -w "$(pwd)" $IMAGE "$@"
